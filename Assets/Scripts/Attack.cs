15584a7240605a234ca632af96d7c6c9507d501b
GameDev21121 <lucaulema@gmail.com>
2020-02-17 13:37:11 +0000

    Merged assets together

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Animator animator;
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public LayerMask onlyEnemies;
+    public Transform attackPosL;
+    public Transform attackPosR;
+
+    public float attackRangeX;
+    public float attackRangeY;
+    public int damage;
+
+    public bool facingRight = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            //Movement
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                facingRight = true;
+            }
+            else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                facingRight = false;
+            }
+
+            //allow player to atack
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] damageEnemiesL = Physics2D.OverlapBoxAll(attackPosL.position, new Vector2(attackRangeX, attackRangeY), 0, onlyEnemies);
+
+                Collider2D[] damageEnemiesR = Physics2D.OverlapBoxAll(attackPosR.position, new Vector2(attackRangeX, attackRangeY), 0, onlyEnemies);
+
+                for (int i = 0; i < damageEnemiesL.Length; i++)
+                {
+                    if (facingRight == false)
+                    {
+                        damageEnemiesL[i].GetComponent<Enemy>().takeDamage(damage);
+                    }
+                }   
+
+                for (int i = 0; i < damageEnemiesR.Length; i++)
+                {
+                    if (facingRight == true)
+                    {
+                        damageEnemiesR[i].GetComponent<Enemy>().takeDamage(damage);
+                    }
+                }
+
+                timeBtwAttack = startTimeBtwAttack;
+                animator.SetBool("Attack",true);
+            }
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+            //Invoke("stopAttack", 0.2f);
+            animator.SetBool("Attack", false);
+        }
+    }
+
+    void stopAttack()
+    {
+        animator.SetBool("Attack", false);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(attackPosL.position, new Vector3(attackRangeX, attackRangeY, 1));
+        Gizmos.DrawWireCube(attackPosR.position, new Vector3(attackRangeX, attackRangeY, 1));
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public int health = 10;
+    public float knockbackForce = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Debug.Log(this.gameObject.name + " died");
+            Destroy(gameObject);
+        }
+    }
+
+    public void takeDamage(int damage)
+    {
+        knockbackEnemy();
+        health -= damage;
+        Debug.Log("Damage taken: -" + damage + " health");
+    }
+    
+    void knockbackEnemy()
+    {
+        //knock enemy back towards the right
+        rb2d.velocity = new Vector2(knockbackForce, rb2d.velocity.y);
+    }
+}
