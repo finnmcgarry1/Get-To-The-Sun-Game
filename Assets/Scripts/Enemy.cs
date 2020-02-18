5f384d5ace5a6a1ef6b5e58bedb4ccb3aeab8233
robdmc34 <robdmc34@gmail.com>
2020-02-18 12:52:49 +0000

    Attempt at enemy health, change healthbar to a seperate script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,54 +6,25 @@ public class Enemy : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     public float knockbackForce = 2f;
-    public GameObject fbar, hbar, ebar;
-    public static int health;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        health = 2;
-        fbar.gameObject.SetActive(true);
-        hbar.gameObject.SetActive(false);
-        ebar.gameObject.SetActive(false);
+        EnemyHealthBar.health = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
        
-        if (health > 2)
-        {
-            health = 2;
-        }
-
-        switch (health)
-        {
-            case 2:
-                fbar.gameObject.SetActive(true);
-                hbar.gameObject.SetActive(false);
-                ebar.gameObject.SetActive(false);
-                break;
-
-            case 1:
-                fbar.gameObject.SetActive(false);
-                hbar.gameObject.SetActive(true);
-                ebar.gameObject.SetActive(false);
-                break;
-            case 0:
-                Debug.Log(this.gameObject.name + "died");
-                fbar.gameObject.SetActive(false);
-                hbar.gameObject.SetActive(false);
-                ebar.gameObject.SetActive(true);
-                break;
-        }
+       
     }
 
     public void takeDamage(int damage)
     {
         //knockbackEnemy();
-        health -= damage;
+        EnemyHealthBar.health -= damage;
         Debug.Log("Damage taken: -" + damage + " health");
     }
     
--- Assets/Scripts/EnemyHealthBar.cs
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -10,7 +10,6 @@ public class EnemyHealthBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        health = 3;
         fbar.gameObject.SetActive(true);
         hbar.gameObject.SetActive(false);
         ebar.gameObject.SetActive(false);
