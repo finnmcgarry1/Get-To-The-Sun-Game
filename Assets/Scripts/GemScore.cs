b0a92ad32c6cc74b71719c4976cb94a5329befc9
robdmc34 <robdmc34@gmail.com>
2020-02-20 14:40:01 +0000

    added gems, gold, torches, and healing hearts

--- Assets/Scripts/GemScore.cs
+++ Assets/Scripts/GemScore.cs
@@ -2,23 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealPlayer : MonoBehaviour
+public class GemScore : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D col)
     {
 
         if (col.collider.tag == "Player")
         {
-            if(GameLogicScript.health < 6)
-            {
-                GameLogicScript.health += 2;
-            }
-            else
-            {
-                GameScore.AddScore(100);
-            }
+            GameScore.AddScore(1000);
             this.gameObject.SetActive(false);
         }
 
     }
+    
+     
 }
--- Assets/Scripts/GoldScore.cs
+++ Assets/Scripts/GoldScore.cs
@@ -2,21 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealPlayer : MonoBehaviour
+public class GoldScore : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D col)
     {
 
         if (col.collider.tag == "Player")
         {
-            if(GameLogicScript.health < 6)
-            {
-                GameLogicScript.health += 2;
-            }
-            else
-            {
-                GameScore.AddScore(100);
-            }
+            GameScore.AddScore(100);
             this.gameObject.SetActive(false);
         }
 
