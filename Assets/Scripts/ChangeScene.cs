8d66a7329519bb3e221a507d2f88c828ae7a3ca8
Zoule Zou <zoulei1999@gmail.com>
2020-02-25 15:13:27 +0000

    scene change

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public int index;
+    public string LevelName;
+
+    void OnCollisionEnter2D(Collision2D other){
+        
+        if(other.collider.tag == "Player"){
+            //Load levels by index
+            SceneManager.LoadScene(index);
+
+            //Load levels by LevelName
+            SceneManager.LoadScene(LevelName);
+
+        }
+    }
+}
